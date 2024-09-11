a7a5113ecb456d6fc37ad82309ea390e8fc9c3bb
mbudis23 <muhammadbudisetiawan@mail.ugm.ac.id>
2024-09-11 23:01:50 +0700

    feat: Navigation Bar Component for Dashboard in Client

---
+++ client/src/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import NavigationBar from "@/components/organisms/navigationBar";
+
+export default function DashboardPage()
+{
+    return(
+        <main className="min-w-full min-h-screen bg-black flex items-center justify-center font-montserrat">
+            <div>
+                <NavigationBar/>
+            </div>
+        </main>
+    )
+}
---
+++ client/src/components/organisms/navigationBar.tsx
@@ -0,0 +1,8 @@
+export default function NavigationBar()
+{
+    return(
+        <nav className="bg-white border border-black min-h-full min-w-[288px] my-[36px] text-black">
+            as
+        </nav>
+    )
+}
