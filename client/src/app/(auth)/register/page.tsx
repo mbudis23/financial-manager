4fd0a6cfe049c632d4812b5b7eed870bc946530f
mbudis23 <muhammadbudisetiawan@mail.ugm.ac.id>
2024-09-11 23:00:18 +0700

    feat: Add Register Component to Client

---
+++ client/src/app/(auth)/register/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import RegisterModal from '@/components/molecules/modals/registerModal';
+export default function RegisterPage()
+{
+    
+    return(
+        <main 
+        className="min-h-screen min-w-full flex items-center justify-center font-montserrat bg-black"
+        >
+            <RegisterModal/>
+        </main>
+    )
+}
---
+++ client/src/components/atoms/primaryButton.tsx
@@ -0,0 +1,17 @@
+export default function PrimaryButton({
+    displayText="Button",
+    onClick=()=>{
+        alert("Not Set Yet")
+    }
+})
+{
+    return(
+        <button 
+        type='button'
+        className='px-[16px] py-[12px] bg-black text-white rounded-[6px] hover:underline font-medium border border-black'
+        onClick={onClick}
+        >
+            {displayText}
+        </button>
+    )
+}
